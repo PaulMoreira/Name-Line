4956359b6e25b3593204c9241a98ec2889b358a5
Paul Moreira <pgmoreira23@gmail.com>
2023-04-16 07:04:22 -0400

    Changed index file name.

---
+++ script.js
@@ -0,0 +1,174 @@
+// script.js
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+
+// Configure Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyALqGwEoRE5KUPZx2EHopUdrqspGz281VU",
+  authDomain: "name-line.firebaseapp.com",
+  databaseURL: "https://name-line-default-rtdb.firebaseio.com",
+  projectId: "name-line",
+  storageBucket: "name-line.appspot.com",
+  messagingSenderId: "993693428714",
+  appId: "1:993693428714:web:9c859f9af6fac6f4b66093",
+  measurementId: "G-XLLXBBFDSE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+// Reference to questions in the database
+const questionsRef = ref(database, 'questions');
+
+// Set up variables
+var startButton = document.getElementById('start-game-btn');
+var questionContainer = document.getElementById('question-container');
+var gameContainer = document.getElementById('game-container');
+var questionEl = document.getElementById('question');
+var choiceContainer = document.getElementById("choices-container");
+var resultEl = document.getElementById("result");
+var scoreEl = document.getElementById('score');
+var timerEl = document.getElementById('timer');
+
+var currentQuestionIndex;
+var score = 0;
+var timerIntervalId;
+var questions;
+
+// Set up event listener for start button
+document.addEventListener("DOMContentLoaded", function () {
+  startButton.addEventListener("click", startGame);
+});
+
+// Function to start the game
+function startGame() {
+  // Hide the start button 
+    document.getElementById('start-game-btn').classList.add('hide');
+    
+  
+  
+  // Show the question container
+  questionContainer.classList.remove('hide');
+  gameContainer.classList.remove('hide');
+  timerEl.classList.remove('hide');
+  scoreEl.classList.remove('hide');
+
+  // Initialize variables
+  currentQuestionIndex = 0;
+  score = 0;
+
+  // Display the first question
+  displayQuestion();
+
+  // Start the countdown timer
+  startCountdownTimer();
+}
+
+// Function to display the question
+function displayQuestion() {
+  // Get the current question from the database
+  get(questionsRef).then(function (snapshot) {
+    questions = snapshot.val();
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // Display the question
+    questionEl.textContent = currentQuestion.question;
+  
+    // Remove existing answer buttons
+    while (choiceContainer.firstChild) {
+      choiceContainer.removeChild(choiceContainer.firstChild);
+    }
+
+    // Display the answer buttons
+    currentQuestion.choices.forEach(function (choice, index) {
+      var button = document.createElement("button");
+      button.textContent = choice;
+      button.classList.add("choice-btn");
+      button.dataset.answer = index;
+      button.addEventListener("click", function () {
+        checkAnswer(currentQuestion.answer, parseInt(button.dataset.answer));
+      });
+      choiceContainer.appendChild(button);
+    });
+  });
+}
+
+// Function to start the countdown timer
+function startCountdownTimer() {
+  // Set the timer to 60 seconds
+  var time = 60;
+
+  // Display the initial timer value
+  timerEl.textContent = time;
+
+// Start the countdown
+timerIntervalId = setInterval(function() {
+  // Decrement the timer
+  time--;
+
+  // Display the updated timer value
+  timerEl.textContent = time;
+
+  // Check if the time has run out
+  if (time <= 0) {
+    // Stop the timer
+    clearInterval(timerIntervalId);
+
+    // End the game
+    endGame();
+  }
+}, 1000);
+}
+
+// Function to check if the user's answer is correct
+function checkAnswer(correctAnswer, userAnswer) {
+  
+  // Check if the answer is correct
+  if (correctAnswer == userAnswer) {
+
+    //Display to user that the answer is correct
+    resultEl.classList.remove('hide');
+    resultEl.textContent = 'Correct!';
+    
+    //Hide the result after 2 second
+    setTimeout(function(){
+      resultEl.classList.add('hide');
+    }, 2000);
+    
+    // Add 2 point to the score
+    score += 2;
+    
+  } else { 
+    resultEl.classList.remove('hide');
+    resultEl.textContent = 'Incorrect!';
+    //Hide the result after 2 second
+    setTimeout(function(){
+      resultEl.classList.add('hide');
+    }, 2000);
+    
+  } 
+  // Update the score
+    scoreEl.textContent = score;
+    
+// Display the next question
+currentQuestionIndex++;
+if (currentQuestionIndex < questions.length) {
+  displayQuestion();
+} else {
+  // End the game
+  endGame();
+}
+}
+
+// Function to end the game
+function endGame() {
+// Stop the timer
+clearInterval(timerIntervalId);
+startButton.innerText = 'Restart';
+startButton.classList.remove('hide');
+questionContainer.classList.add('hide');
+}
